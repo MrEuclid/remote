30c86b0823248ffd85de511486778030265b2718
MrEuclid <john@teacherjohn.org>
2024-07-20 11:43:30 +0800

    Add files via upload

---
+++ DHT11_pi.py
@@ -0,0 +1,49 @@
+# Python code - read serial port4
+
+import serial
+import time
+import requests
+from datetime import datetime
+
+#port = "/dev/tty.usbmodem14101"
+#port = "/dev/ttyACM0"
+port = "/dev/ttyACM1"
+# port = "COM4"
+baud = 9600
+
+ser = serial.Serial(port, baud, timeout=0)
+
+# default timeout = none
+# posts dweets every minute
+while True:
+    sum = 0;
+    cnt = 0 ;
+    if ser.in_waiting:
+        time.sleep(1)
+        while cnt < 20:
+            time.sleep(3)
+            temp = ser.readline().strip()
+            tempstring = temp.decode("utf-8")
+            # maximum of 2 elements
+            try:
+                x = tempstring.split(",",1)
+                temp = x[0]
+                humid = x[1]
+                print(tempstring,temp,humid)
+            except IndexError:
+                print("Error: Index is out of range.")
+            x = tempstring.split(",",1)
+           
+        now = datetime.now()
+        hour = now.strftime("%H")
+        minute = now.strftime("%M") 
+        t = hour +'-'+minute
+        print(temp,humid)
+        #url = "https://dweet.io/dweet/for/DHT11_1737?temp=" + tempstr + "&humid=" + h +"&time="+t
+        print(now);
+        print(url)
+        print(t)
+        requests.get(url)
+        time.sleep(5)
+
+
