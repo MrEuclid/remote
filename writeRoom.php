cc20d0d6649e6eba852159cf297dc1bda86440e1
MrEuclid <john@teacherjohn.org>
2024-07-20 15:48:43 +0800

    Add files via upload

--- writeRoom.php
+++ writeRoom.php
@@ -3,28 +3,41 @@
 include "connectDatabaseRemote.php";
 
 // $ymd = $_POST["yearMonthDay"];
+
  $hm = $_POST["hourMinute"];
  $temp = $_POST["temperature"];
+ $thing = $_POST["thing"];
 
 
+/*
+ $hm = "17-34";
+ $temp = 34.397;
+ $thing = 'LM35_1737';
 
-// $hm = "12-34";
-// $temp = 34.397;
+*/
+// echo "writing check";
 
-
-
-$output = [];
+ $query = "SELECT max(hourMinute) AS M from myRoom";
+ $result = mysqli_query($dbServer,$query);
+ $data = mysqli_fetch_row($result);
+ $m = $data[0];
+// echo "<br>" . $query . "<br>";
+// echo $hm . " m " .$m;
+if ($hm > $m)
+{$output = [];
 $i = 0;
-$query = "INSERT INTO myRoom (hourMinute,temperature) 
-VALUES ('$hm' ,'$temp')";
+$query = "INSERT INTO myRoom (thing,hourMinute,temperature) 
+VALUES ('$thing','$hm' ,'$temp')";
 
 // echo "<br>" . $query . "<br>";
 
 mysqli_query($dbServer,$query);
 
-
-
-exit();
+ $d = "OK" ;
+}
+else
+	{$d = "NO";}
+echo json_encode($d);
 
 
 ?>
