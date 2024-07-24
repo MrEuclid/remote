05910f20d30e6ccae2a7504efdc2a20fcfa49a19
MrEuclid <john@teacherjohn.org>
2024-07-24 16:02:47 +0800

    Add files via upload

---
+++ checkTable.php
@@ -0,0 +1,24 @@
+<?php
+
+include "../connectTempleDB.php";
+
+// $name = $_POST['sensorName'];
+//$name = "1234A";
+// read database and export for use in plotly
+
+$query = "SELECT *  FROM arduinoSensor  WHERE sensorName = '$name' ";
+
+// echo "<br>" . $query . "<br>";
+
+
+$result = mysqli_query($dbServer,$query);
+$n = mysqli_num_rows($result);
+echo $n;
+//$myJSON = json_encode($n);
+// echo "<br>";
+//echo $myJSON;
+// echo "<br>";
+
+exit();
+
+?>
