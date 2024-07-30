7ff0342599c98b6242907e85c3efb80d8a32003a
MrEuclid <john@teacherjohn.org>
2024-07-30 11:01:53 +0800

    Add files via upload

--- indexRemote.php
+++ indexRemote.php
@@ -8,7 +8,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://cdn.plot.ly/plotly-2.27.0.min.js" charset="utf-8"></script>
-  <<link href = "remoteStyles.css"  rel = "stylesheet">
+  <link href = "remoteStyles.css"  rel = "stylesheet">
 
     <title>Remote</title>
 
@@ -32,6 +32,8 @@
             }
 input {margin-right:1em;}
   .site {color:green; background-color: skyblue;}
+
+  h1 {display: inline-block;}
 }
 </style>
 
@@ -41,37 +43,51 @@ input {margin-right:1em;}
   
     <div class  = "container-fluid">
       <div class = "row justify-content-center">
-        <h1 class = "c"> <a href = "../index.php">Home</a> Remote data</h1>
+        <h1 class = "c"> Remote data</h1> <a href = "../index.php">Home</a>
       </div></div>
+ 
       <div class = "row justify-content-center">
         <div class = "col-12 c">
+  <!--
   <h2 class = "c">  Read data</h2>
-
-  <button class = "site" id = "serial_LM35">LM35 - DHT11</button>
+-->
+    <button id = "readData">Read data</button>
+         <div id = "read">
+    <button class = "site" id = "serial_LM35">LM35 - DHT11</button>
     <button class = "site" id = "serial_HR204">ultrasonic</button>
-    <!--
-   <button class = "site" id = "serial_counting">Counting</button>
- -->
-   <button class = "site" id = "LM35_1737">DWEET</button></a>
+   <button class = "site" id = "LM35_1737">DWEET</button>
 </div></div>
+</div> <!-- read -->
 
 
  <div class = "row justify-content-center">
 <div class = "col-12 c">
-
+<!--
 <h2 clas s= "c">View sensor data</h2>
-<button class = "site" id =  "retrieveData" >View sensors</button></a>
+-->
+<button id =  "retrieveData" >View sensors</button>
 </div></div>
+
  <div class = "row justify-content-center">
 <div class = "col-12 c">
-  <h3>Site</h3>
+
 <div id = "webPage"></div>
 </div></div>
+</div>
 </div>
 
 </body>
 </html>
+<script>
+    $(document).ready(function(){
+    
+        $('#readData').show();
+        $('#retrieveData').show();
+        $('.site').hide();
+        $('#webPage').hide();
+ })
 
+  </script>
 
 <script>
     $(document).ready(function(){
@@ -79,8 +95,43 @@ input {margin-right:1em;}
           let click = this.id;
           console.log(click + ".html");
           let url = click + ".html"
-          alert(click);
-$('#webPage').load(url);
+       //   alert(click);
+          $('#webPage').load(url).show();
  })
 })
-  </script>}
+  </script>
+
+<script>
+    $(document).ready(function(){
+        $("#readData").on('click', function(){
+      //    alert(this.id);
+        $('#read').show();
+        $('#view').hide();
+        $('.site').show();
+        $('#readData').hide();
+        $('#retrieveData').show();
+        $('#webPage').hide();
+        
+
+ })
+})
+  </script>
+
+  <script>
+    $(document).ready(function(){
+        $("#retrieveData").on('click', function(){
+           let click = this.id;
+          console.log(click + ".html");
+          let url = click + ".html"
+       //   alert(click);
+          $('#webPage').load(url).show();
+      //     alert(this.id);
+        $('#read').hide();
+         $('#view').show();
+        $('#readData').show();
+        $('#retrieveData').hide();
+        
+ })
+})
+  </script>
+
